8c9f1f36e74df133ee7097927f8a0ac5c87b0911
Parvej <mdp33910@gmail.com>
2025-02-15 17:51:42 +0600

    'creat-tamplat'

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyAx68DCbSxiGqsVfb0hbj1Tg_pfr__jdNU',
+  authDomain: 'conseption-12.firebaseapp.com',
+  projectId: 'conseption-12',
+  storageBucket: 'conseption-12.firebasestorage.app',
+  messagingSenderId: '418024862532',
+  appId: '1:418024862532:web:3629ee6252afc39d8d1d11',
+};
+
+export const app = initializeApp(firebaseConfig);
